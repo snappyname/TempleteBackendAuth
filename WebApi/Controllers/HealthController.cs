5ef5b2996f320d62a6b44b5dadcd829465a557ab
snapp <>
2026-01-16 09:44:43 +0100

    Try to deploy
    (10)

---
+++ WebApi/Controllers/HealthController.cs
@@ -0,0 +1,38 @@
+﻿using Dal;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TemplateWebApi.Controllers.Base;
+
+namespace TemplateWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/health")]
+    public class HealthController : BaseController
+    {
+        private readonly AppDbContext _dbContext;
+
+        public HealthController(AppDbContext context) : base(context)
+        {
+        }
+
+        [AllowAnonymous]
+        [HttpGet("status")]
+        public async Task<IActionResult> GetStatus()
+        {
+            return Ok();
+        }
+
+        [AllowAnonymous]
+        [HttpGet("db")]
+        public async Task<IActionResult> GetDbStatus()
+        {
+            if (await _dbContext.Users.AnyAsync())
+            {
+                return Ok();
+            }
+
+            return NotFound();
+        }
+    }
+}
